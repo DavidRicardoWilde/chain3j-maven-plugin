ba8952a0958c46317b626d4ee303cc9d7e259507
JonesWilde <davidgluttongk@outlook.com>
2018-12-27 11:24:22 -0800

    Fixing bug
    The plugin was packaged and installed successfully.
    Calling the method in plugin failed.
    The problem may appear in the SolidityFunctionWrapper or sol script.

---
+++ src/main/java/io/moac/solidity/CompilerResult.java
@@ -0,0 +1,20 @@
+package io.moac.solidity;
+//Container for compile result
+
+
+public class CompilerResult {
+    public String errors;
+    public String output;
+    private boolean success = false;
+
+    public CompilerResult(String errors, String output, boolean success) {
+        this.errors = errors;
+        // https://ethereum.stackexchange.com/questions/11912/unable-to-define-greetercontract-in-the-greeter-tutorial-breaking-change-in-sol
+        this.output = output.replaceAll("<stdin>:", "");
+        this.success = success;
+    }
+
+    public boolean isFailed() {
+        return !success;
+    }
+}
